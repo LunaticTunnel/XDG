92925ee11ed457301febf65962bc277299ba6f75
LunaticTunnel <153975030+LunaticTunnel@users.noreply.github.com>
2024-01-07 22:20:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-clear
-echo -e ""
-echo -e ""
-banner="\033[96;1mSCRIPT AUTO INSTALL VPS    \033[0m"
-banner2="\033[96;1m     INSTALL SCRIPT   \033[0m"
-line="\033[95;1m ──────────────────────────────────────────── \033[0m"
-#
-echo -ne """
-         $line
-                     $banner
-         $line
-     """
- echo -e "\033[93;1m wait 3 seccond....\033[0m"
- sleep 3
- clear
- echo -e ""
- echo -e "\033[93;1 wait in 4 sec....\033[0m"
- sleep 4
- clear
-echo -ne """
-         $line
-                     $banner2
-         $line
-         """
-         echo -e ""
-              
+clear             
 # // INSTALL SCRIPTED
-apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/LunaticTunnel/XDG/_/_____main_____/__.sh && chmod +x __.sh && ./__.sh
+apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/LunaticTunnel/profile/setup/XDG/__.sh && chmod +x __.sh && ./__.sh
