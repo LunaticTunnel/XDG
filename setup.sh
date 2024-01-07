86a9c5b93c28473b425874799532bc860e4adbb2
LunaticTunnel <153975030+LunaticTunnel@users.noreply.github.com>
2024-01-07 22:28:43 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-clear             
+clear
+echo -e ""
+echo -e ""
+echo -e "                 \033[41;97;1mINSTALL\033[0m"
+echo -e ""
+echo -e ""
 # // INSTALL SCRIPTED
 apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/LunaticTunnel/profile/setup/XDG/__.sh && chmod +x __.sh && ./__.sh
