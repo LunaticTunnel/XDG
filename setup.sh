6c96c7db0ba291e971da76bb1dd6dd73aa1f4cbb
LunaticTunnel <153975030+LunaticTunnel@users.noreply.github.com>
2024-01-07 22:02:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,23 +2,25 @@
 clear
 echo -e ""
 echo -e ""
-banner="\033[96;1m     SCRIPT AUTO INSTALL VPS    \033[0m"
-banner="\033[96;1m              INSTALL SCRIPT   \033[0m"
+banner="\033[96;1m    SCRIPT AUTO INSTALL VPS    \033[0m"
+banner="\033[96;1m     INSTALL SCRIPT   \033[0m"
 line="\033[95;1m ──────────────────────────────────────────── \033[0m"
 #
 echo -ne """
-              $line
-                         $banner
-              $line
+         $line
+                     $banner
+         $line
      """
+ echo -e "\033[93;1m wait 3 seccond....\033[0m"
+ sleep 3
  echo -e ""
  echo -e "\033[93;1 wait in 4 sec....\033[0m"
  sleep 4
  clear
 echo -ne """
-             $line
-                         $banner2
-             $line
+         $line
+                     $banner2
+         $line
          """
          echo -e ""
               
